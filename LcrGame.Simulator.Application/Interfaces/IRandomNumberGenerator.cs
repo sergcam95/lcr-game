703fe75472cc99bc8054c429866d43f4216211de
root <root@SERGIOCAMAC3D4C.localdomain>
2021-05-08 13:31:13 -0500

    Random number generator service created for the dice output

---
+++ LcrGame.Simulator.Application/Interfaces/IRandomNumberGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace LcrGame.Simulator.Application.Interfaces
+{
+    public interface IRandomNumberGenerator
+    {
+        int Generate(int start, int end);
+        IEnumerable<int> Generate(int start, int end, int amount);
+    }
+}
---
+++ LcrGame.Simulator.Application/Services/RandomNumberGenerator.cs
@@ -0,0 +1,29 @@
+﻿using LcrGame.Simulator.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace LcrGame.Simulator.Application.Services
+{
+    public class RandomNumberGenerator : IRandomNumberGenerator
+    {
+        private readonly Random _random;
+
+        public RandomNumberGenerator()
+        {
+            _random = new();
+        }
+
+        public int Generate(int start, int end)
+        {
+            return _random.Next(start, end);
+        }
+
+        public IEnumerable<int> Generate(int start, int end, int count)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                yield return _random.Next(start, end);
+            }
+        }
+    }
+}
